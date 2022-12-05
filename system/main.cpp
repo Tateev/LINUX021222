631dbab3caba5c3f4dd22cfd21df018699ee27ea
Tatev <tatev@mail.am>
2022-12-05 21:36:13 +0400

    adding system

---
+++ system/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    int Search(Node* node, int tiv){
+        if(node <= left);
+            Search(node->left)
+};
+
+Node *lca(Node *root, int v1,int v2) {
+}
+
+};
+
+
+
+int main() {
+
+}
+
+//class Node{
+//private:
+//    std::vector <Node> children = {};
+//    Node *parent;
+//
+//public:
+//
+//        Node(Node const &p){
+//            *parent = p;
+//        }
+//        void AddElement(const Node& newel){
+//            children.push_back(newel);
+//        }
+//
+//};
+
+
